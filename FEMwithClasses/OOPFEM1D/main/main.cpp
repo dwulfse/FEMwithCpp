e4e27760a74d6fcc1c570cd0645aa4192497a94e
dwulfse <dylan@wulfse.com>
2025-02-17 20:11:55 +0000

    full Gauss Quadrature implementation

--- FEMwithClasses/OOPFEM1D/main/main.cpp
+++ FEMwithClasses/OOPFEM1D/main/main.cpp
@@ -15,57 +15,70 @@
 
 int main()
 {
-	// std::cout << "testing lobatto:" << std::endl;
-	// for (int i=0; i<10; i++)
+	// // std::cout << "testing lobatto:" << std::endl;
+	// // for (int i=0; i<10; i++)
+	// // {
+	// // 	std::cout << "lobatto(" << i << ", 0.0) = " << lobatto(i, 0.0) << std::endl;
+	// // }
+
+	// const int n = 4; // number of elements
+	// const int polynomialDegree = 2; // polynomial degree
+	// // const int dimension = 1; // dimension
+	// auto f = [](double x) { return M_PI * M_PI * sin(M_PI * x); }; // RHS function
+	// auto f_analytic = [](double x) { return sin(M_PI * x); }; // analytic solution
+
+	// // boundary conditions
+	// double u0 = 0.0;
+	// double u1 = 0.0;
+	// bool boundary_u0 = true;
+	// bool boundary_u1 = true;
+
+	// FE_Solution FEM(n, polynomialDegree);
+	// std::vector<double> solution = FEM.solve(f);
+
+	// std::cout << "u vector: " << std::endl;
+	// for (int i=0; i<polynomialDegree*n+1; i++)
 	// {
-	// 	std::cout << "lobatto(" << i << ", 0.0) = " << lobatto(i, 0.0) << std::endl;
+	// 	std::cout << "u" << i << " = " << solution.at(i) << std::endl;
 	// }
 
-	const int n = 4; // number of elements
-	const int polynomialDegree = 2; // polynomial degree
-	// const int dimension = 1; // dimension
-	auto f = [](double x) { return M_PI * M_PI * sin(M_PI * x); }; // RHS function
-	auto f_analytic = [](double x) { return sin(M_PI * x); }; // analytic solution
+	// std::cout << "Solution:" << std::endl;
+	// for (int i=0; i<5; i++)
+	// {
+	// 	double x = i / 4.0;
+	// 	std::cout << "u_h(" << x << ") = " << FEM.evaluateSolution(x) << std::endl;
+	// }
+	// // should be [0, 0.09375, 0.125, 0.09375, 0]
 
-	// boundary conditions
-	double u0 = 0.0;
-	double u1 = 0.0;
-	bool boundary_u0 = true;
-	bool boundary_u1 = true;
+	// std::ofstream file("hp_error.csv");
+	// file << "p,h,error\n";
 
-	FE_Solution FEM(n, polynomialDegree);
-	std::vector<double> solution = FEM.solve(f);
+	// for (int p=1; p<=3; p++)
+	// {
+	// 	for (int n=3; n<=20; n++)
+	// 	{
+	// 		FE_Solution FEM(n, p);
+	// 		std::vector<double> solution = FEM.solve(f);
+	// 		double error = FEM.getL2Error(f_analytic);
+	// 		double h = 1.0 / n;
+	// 		file << p << "," << h << "," << error << "\n";
+	// 	}
+	// }
 
-	std::cout << "u vector: " << std::endl;
-	for (int i=0; i<polynomialDegree*n+1; i++)
+	// file.close();
+
+	GaussQuadrature quad;
+	quad.assembleQuadrature(5);
+	std::cout << "points: ";
+	for (int i=0; i<quad.points.size(); i++)
 	{
-		std::cout << "u" << i << " = " << solution.at(i) << std::endl;
+		std::cout << quad.points.at(i) << " ";
 	}
-
-	std::cout << "Solution:" << std::endl;
-	for (int i=0; i<5; i++)
+	std::cout << std::endl << "weights: ";
+	for (int i=0; i<quad.weights.size(); i++)
 	{
-		double x = i / 4.0;
-		std::cout << "u_h(" << x << ") = " << FEM.evaluateSolution(x) << std::endl;
+		std::cout << quad.weights.at(i) << " ";
 	}
-	// should be [0, 0.09375, 0.125, 0.09375, 0]
-
-	std::ofstream file("hp_error.csv");
-	file << "p,h,error\n";
-
-	for (int p=1; p<=3; p++)
-	{
-		for (int n=3; n<=20; n++)
-		{
-			FE_Solution FEM(n, p);
-			std::vector<double> solution = FEM.solve(f);
-			double error = FEM.getL2Error(f_analytic);
-			double h = 1.0 / n;
-			file << p << "," << h << "," << error << "\n";
-		}
-	}
-
-	file.close();
 
 	return 0;
 }
--- FEMwithClasses/OOPFEM1D/source/GaussQuadrature.cpp
+++ FEMwithClasses/OOPFEM1D/source/GaussQuadrature.cpp
@@ -1,4 +1,5 @@
 #include "GaussQuadrature.hpp"
+#include "Polynomial.hpp"
 #include <stdexcept>
 #include <cmath>
 
@@ -20,28 +21,63 @@ GaussQuadrature::~GaussQuadrature()
 
 void GaussQuadrature::assembleQuadrature(int n)
 {
-	if (n == 1)
+	// validate input
+	if (n < 1)
 	{
-		points = {0.0};
-		weights = {2.0};
+		throw std::invalid_argument("Number of quadrature points must be at least 1");
 	}
-	else if (n == 2)
+
+	// resize vectors
+	points.resize(n);
+	weights.resize(n);
+
+	// constants for Newton's method
+	const double tol = 1e-14;
+	const int max_iter = 100;
+
+	// find points and weights
+	for (int i=0; i<(n+1) / 2; i++)
 	{
-		points = {-1/sqrt(3), 1/sqrt(3)};
-		weights = {1.0, 1.0};
-	}
-	else if (n == 3)
-	{
-		points = {-sqrt(3.0/5), 0.0, sqrt(3.0/5)};
-		weights = {5.0/9, 8.0/9, 5.0/9};
-	}
-	else if (n == 4)
-	{
-		points = {-sqrt(3.0/7 + 2.0/7*sqrt(6.0/5)), -sqrt(3.0/7 - 2.0/7*sqrt(6.0/5)), sqrt(3.0/7 - 2.0/7*sqrt(6.0/5)), sqrt(3.0/7 + 2.0/7*sqrt(6.0/5))};
-		weights = {(18 - sqrt(30))/36, (18 + sqrt(30))/36, (18 + sqrt(30))/36, (18 - sqrt(30))/36};
-	}
-	else
-	{
-		throw std::invalid_argument("Invalid number of quadrature points");
+		// initial values for Newton's method
+		double x = cos(M_PI * (i + 0.75) / (n + 0.5)); // initial guess using Chebyshev approximation
+		double x_old;
+		int iter = 0;
+
+		// Newton's method main loop
+		do
+		{
+			x_old = x;
+			x = x_old - legendre(n, x_old) / legendre_deriv(n, x_old);
+			iter++;
+			if (iter >= max_iter)
+			{
+				// throw error if Newton's method did not converge
+				throw std::runtime_error("Newton's method did not converge");
+			}
+		} while (abs(x - x_old) > tol);
+
+		// store points and weights using symmetry of roots
+		double w = 2.0 / ((1.0 - x * x) * pow(legendre_deriv(n, x), 2));
+		points[i] = -x;
+		weights[i] = w;
+		if (i != n - i - 1)
+		{
+			points[n - i - 1] = x;
+			weights[n - i - 1] = w;
+		}
 	}
 }
+
+// target values:
+// n == 1
+// 	points = {0.0};
+// 	weights = {2.0};
+// n == 2
+// 	points = {-1/sqrt(3), 1/sqrt(3)};
+// 	weights = {1.0, 1.0};
+// n == 3
+// 	points = {-sqrt(3.0/5), 0.0, sqrt(3.0/5)};
+// 	weights = {5.0/9, 8.0/9, 5.0/9};
+// n == 4
+// 	points = {-sqrt(3.0/7 + 2.0/7*sqrt(6.0/5)), -sqrt(3.0/7 - 2.0/7*sqrt(6.0/5)), sqrt(3.0/7 - 2.0/7*sqrt(6.0/5)), sqrt(3.0/7 + 2.0/7*sqrt(6.0/5))};
+// 	weights = {(18 - sqrt(30))/36, (18 + sqrt(30))/36, (18 + sqrt(30))/36, (18 - sqrt(30))/36};
